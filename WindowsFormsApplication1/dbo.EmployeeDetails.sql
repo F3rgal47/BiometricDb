abb12a9c0c67cc3fd60d517435ee2f926f23bff1
f3rgal47 <f3rgal47@googlemail.com>
2017-04-27 13:45:09 +0100

    Edit to server connections, code rewrite in most forms, additon of area maintenance

---
+++ WindowsFormsApplication1/dbo.EmployeeDetails.sql
@@ -0,0 +1,17 @@
+﻿CREATE TABLE [dbo].[EmployeeDetails] (
+    [Id]              INT          IDENTITY (1, 1) NOT NULL,
+    [Forename]        VARCHAR (50) NULL,
+    [Surname]         VARCHAR (60) NULL,
+    [Email]           VARCHAR (50) NULL,
+    [TelephoneNo]     INT          NULL,
+    [AccessLevel]     VARCHAR (50) NULL,
+    [BiometricMarker] INT          NULL,
+    [AddressLine1]    VARCHAR (50) NULL,
+    [AddressLine2]    VARCHAR (50) NULL,
+    [AddressLine3]    VARCHAR (50) NULL,
+    [City]            VARCHAR (50) NULL,
+    [Postcode]        VARCHAR (50) NULL,
+    [Photo]           VARBINARY(MAX)        NULL,
+    PRIMARY KEY CLUSTERED ([Id] ASC)
+);
+
